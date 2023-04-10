c42e41eabef64e551052604a5020f498134fc70e
Toby Keech <toby.keech18@gmail.com>
2023-04-10 18:37:44 +0100

    added some notes for my use

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  // useState is a hook and used down below in the return so it can be rendered on the server 
   const [todoitems, setTodoItems] = useState([
     { name : "Buy Shopping", priority: "high", id:1},
     { name : "Clean Bathroom", priority: "low", id:2},
@@ -11,6 +12,9 @@ function App() {
   const [newToDoItem, setNewToDo] = useState(" ");
 
   const [priorityResult, setPriorityResult] = useState("");
+
+  // these are two new useStates which are being labeled for use down below
+  // a use state is an array with two values the current state and a function to update it.
 
   const handleToDoInput = (event) => {
     setNewToDo(event.target.value)
@@ -32,6 +36,11 @@ function App() {
     setPriorityResult(event.target.value)
   }
 
+  // these functions above are handling areas down below in the return
+  // they are using the DOM behind the scenes to access areas 
+  // setNewToDo, setToDoItems,SetPriorityResult are all functions used to update the userState above 
+
+
 return(
 <>
 <h1> To Do List </h1>
@@ -44,8 +53,12 @@ return(
   <span>{todoitem.name}</span>
 
   <span> Priority: {todoitem.priority} </span>
-  
   </li>
+
+  // li are used for a list inside the the ul tag which is an unordered list ie bullet points
+  // line 50 is mapping a new array (same length as previous) which is being handled by the handleToDoSubmit
+
+
   )
 })}
 </ul>
@@ -75,8 +88,13 @@ return(
             />
         
           <label htmlFor="priority">Low</label>
-        <button> Save To Do </button>                      
+        <button> Add To Do </button>             
 </form>
+
+{/* the forms above are used as they were in html before
+    you have a input with type, id, name, value and a onClick if you require it to do something (this is specified in the function above) */}
+{/* think of the htmlFor like the route you had in python such as new */}
+
 </>
 )
 };
