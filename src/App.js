c221f8c23ca6bebb8401f3db236533f6c70f44c4
Toby Keech <toby.keech18@gmail.com>
2023-04-11 10:55:11 +0100

    adding notes

--- src/App.js
+++ src/App.js
@@ -13,15 +13,17 @@ function App() {
 
   const [priorityResult, setPriorityResult] = useState("");
 
-  // these are two new useStates which are being labeled for use down below
+  // these are two new useStates (functions) which are being labeled for use down below
   // a use state is an array with two values the current state and a function to update it.
+  // ie setTodoItems is the function used to update it 
 
-  const handleToDoInput = (event) => {
-    setNewToDo(event.target.value)
+  const handleToDoInput = (theClaw) => {
+    setNewToDo(theClaw.target.value)
   }
-
-  const handleToDoSubmit = (event) => {
-    event.preventDefault();
+  // reaches and grabs the event that is given by the browser ie event but this has been changed to theClaw-its just a name!
+  // seee line 38 and 39 for the event talking about above
+  const handleToDoSubmit = (theClaw) => {
+    theClaw.preventDefault();
     const copyToDos = [...todoitems]
     copyToDos.push({
       name : newToDoItem,
@@ -30,6 +32,7 @@ function App() {
     })
     setTodoItems(copyToDos)
     setNewToDo(" ")
+    setPriorityResult("")
   }
 
   const handlePriorityInput = (event) => {
@@ -42,17 +45,47 @@ function App() {
 
 
 return(
-<>
+<div className='App'>
 <h1> To Do List </h1>
+
+<form onSubmit = {handleToDoSubmit}>        
+        <label htmlFor="new-todo"> Add a new task: </label>   
+        <input type="text" id="new-todo" onChange= {handleToDoInput} value= {newToDoItem}/> 
+
+        <span>Priority</span>
+
+        <label htmlFor="priority">High</label>
+        <input
+            type="radio"
+            id="high"
+            name="priority"
+            value="high"
+            checked={priorityResult === "high"}
+            onChange = {handlePriorityInput}
+            />
+        
+         
+          <label htmlFor="priority">Low</label>
+          <input
+            type="radio"
+            id="low"
+            name="priority"
+            value="low"
+            checked={priorityResult === "low"}
+            onChange = {handlePriorityInput}
+            />
+        
+        <button> Save Item </button>             
+</form>
 
 
 <ul>
 {todoitems.map(todoitem => {
   return(
-  <li key = {todoitem.id}>
+  <li className = {todoitem.priority} key ={todoitem.id}>
   <span>{todoitem.name}</span>
 
-  <span> Priority: {todoitem.priority} </span>
+  <span> </span> 
   </li>
 
   // li are used for a list inside the the ul tag which is an unordered list ie bullet points
@@ -63,39 +96,13 @@ return(
 })}
 </ul>
 
-<form onSubmit = {handleToDoSubmit}>        
-        <label htmlFor="new-todo"> Add a new task: </label>   
-        <input type="text" id="new-todo" onChange= {handleToDoInput} value= {newToDoItem}/> 
 
-        <span>Priority</span>
-
-        <input
-            type="radio"
-            id="high"
-            name="high"
-            value="high"
-            onClick = {handlePriorityInput}
-            />
-        
-          <label htmlFor="priority">High</label>
-      
-          <input
-            type="radio"
-            id="low"
-            name="low"
-            value="low"
-            onClick = {handlePriorityInput}
-            />
-        
-          <label htmlFor="priority">Low</label>
-        <button> Add To Do </button>             
-</form>
 
 {/* the forms above are used as they were in html before
     you have a input with type, id, name, value and a onClick if you require it to do something (this is specified in the function above) */}
 {/* think of the htmlFor like the route you had in python such as new */}
 
-</>
+</div>
 )
 };
 
